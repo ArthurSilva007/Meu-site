f1a59086062c33e3d7f9d1dcb608dce5b8ed65ec
Arthur Silva007 <andersonarthur740@gmail.com>
2024-12-09 01:52:38 -0300

    updates skills

--- js/Script.js
+++ js/Script.js
@@ -15,3 +15,21 @@ function toggleMenu() {
     nav.classList.toggle('active'); // Mostra ou oculta o menu
 }
 /******  49de083e-0356-4ac6-a8c9-851d8b2f28b3  *******/
+
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('.tab-button');
+    const contents = document.querySelectorAll('.tab-content');
+  
+    buttons.forEach(button => {
+      button.addEventListener('click', () => {
+        // Remover classe 'active' de todos os botões e conteúdos
+        buttons.forEach(btn => btn.classList.remove('active'));
+        contents.forEach(content => content.classList.remove('active'));
+  
+        // Adicionar classe 'active' ao botão e conteúdo correspondente
+        button.classList.add('active');
+        const tab = button.getAttribute('data-tab');
+        document.getElementById(tab).classList.add('active');
+      });
+    });
+  });
